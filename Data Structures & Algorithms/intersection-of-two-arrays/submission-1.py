3416e6deb6562903bf88dd5f8c3a4e17d20191b3
shrutig10 <144508498+shrutig10@users.noreply.github.com>
2026-04-14 18:32:43 -0500

    Add: intersection-of-two-arrays - submission-1

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # make one list into a set --> O(1) lookup time
+        # iterate through the other array (with duplicates taken out) and see if its values are in the first set
+        # add to an array
+
+        set1 = set(nums1)
+        list2 = set(nums2)
+        res = []
+
+        for num in list2:
+            if num in set1:
+                res.append(num)
+
+        return res
